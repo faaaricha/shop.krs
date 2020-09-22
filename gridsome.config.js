6e554c0043cdb5092df3c3d124e46c539fa80df4
A Faricha Mantika <57094474+faaaricha@users.noreply.github.com>
2020-09-22 17:31:08 +0700

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "Shop — Kedai Resensi Surabaya",
+  siteUrl: `https://shopkrs.netlify.app`,
   host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  titleTemplate: "%s",
+  siteDescription: "Katalog Terbitan KRS",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
